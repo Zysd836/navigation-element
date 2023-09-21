9d25baa6bc34d3ca51e5c15b0f66f8e5de4fc696
hoangnv <viethoangd836@gmail.com>
2023-09-22 02:18:51 +0700

    feat(logic): navigation when press arrowKey

---
+++ src/utils/constant.ts
@@ -0,0 +1,10 @@
+export enum CodeArrowKey {
+  ARROW_LEFT = 'ArrowLeft',
+  ARROW_UP = 'ArrowUp',
+  ARROW_RIGHT = 'ArrowRight',
+  ARROW_DOWN = 'ArrowDown',
+}
+export const activeClass = {
+  ACTIVE_CATEGORIES: 'active-category',
+  ACTIVE_ITEMV: 'active-itemv',
+}
